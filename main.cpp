bbc854f912a4f54160962f959161f0d6d54bd222
Saul Anaya <anayarojas2005@gmail.com>
2026-07-14 16:18:31 -0500

    agregar primer bloque, tipo de paciente

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
-#include<iostream>
+#include <iostream>
 #include <string>
 #include <fstream>
-
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
+int main(){
+    srand(time(0));
+    int tipoP;
+
+    cout << "    SIMULACION DE ATENCION HOSPITALARIA    " << endl;
+
+    cout << "Tipo de Paciente:" << endl;
+    cout << "1. Paciente con Seguro" << endl;
+    cout << "2. Paciente Regular" << endl;
+    cout << "Seleccione una opcion (1 o 2): ";
+    cin >> tipoP;
+
+}
