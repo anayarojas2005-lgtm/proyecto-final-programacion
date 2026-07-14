7fd91254ae4c370171a619df017c5fa5f1663636
Saul Anaya <anayarojas2005@gmail.com>
2026-07-14 16:26:02 -0500

    primer bloque, generacion de ticket.txt

--- main.cpp
+++ main.cpp
@@ -17,4 +17,22 @@ int main(){
     cout << "Seleccione una opcion (1 o 2): ";
     cin >> tipoP;
 
+    // PRIMER TXT: GENERACION DE TICKET PARA ATENCION EN VENTANA
+
+    int numeroTicket = rand() % 9000 + 1000; // Número aleatorio de 4 digitos {{{{POSIBLE PREGUNTA}}}}
+    int ventanaAsignada = rand() % 5 + 1;    // Ventana aleatoria entre 1 y 5 
+
+    ofstream archivoTicket("ticket_de_atencion.txt");
+
+    if (archivoTicket.is_open()) {
+        archivoTicket << "TICKET DE ATENCION" << endl;
+        archivoTicket << "Tipo de Paciente: " << (tipoP == 1 ? "Con Seguro" : "Regular") << endl; // {{{{POSIBLEMENTE CAMBIE ESTA PARTE PARA UTILIZAR OBJETOS}}}}
+        archivoTicket << "dirigase a la ventana :" << ventanaAsignada << endl;
+        archivoTicket.close();
+        cout << "\n[+] Se ha generado su ticket en '1_ticket.txt'." << endl;
+    } else {
+        cout << "Error al crear el archivo del ticket." << endl;
+        return 1;
+    }
+
 }
